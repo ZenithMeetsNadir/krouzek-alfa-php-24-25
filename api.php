3d777fb78ce568df8e0fa6dd1a4f9a09cee00658
MartinVlcek <martin-vlcek@post.cz>
2024-10-31 19:55:06 +0100

    31. 10. 24

--- api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Content-Type: application/json');
-header('HTTP/1.1: 200 OK');
-
-echo json_encode([
-    'first' => ':3',
-    'second' => 'OwO',
-]);
--- index.php
+++ index.php
@@ -5,9 +5,3 @@ require "vendor/autoload.php";
 function dd(mixed $var) {
     \Tracy\Debugger::dump($var);
 }
-
-$client = new \GuzzleHttp\Client();
-
-$response = $client->request('GET', 'localhost/root/api.php');
-
-dd(json_decode($response->getBody()->getContents()), true);
