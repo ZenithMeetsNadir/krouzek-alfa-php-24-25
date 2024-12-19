8b27a8f52ca3495747395b1be22be57ad1b888ca
MartinVlcek <martin-vlcek@post.cz>
2024-12-19 19:58:50 +0100

    router se stará o routing

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -4,12 +4,16 @@ namespace App\Controller;
 
 use App\View\View;
 
-class BaseController {
+abstract class BaseController {
 
-    public string $defaultAction = "index";
+    public string $defaultAction = 'index';
     protected View $view;
 
     public function __construct() {
         $this->view = new View();
+    }
+
+    public function __toString() {
+        return get_class($this);
     }
 }
---
+++ src/Router.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App;
+
+use App\Controller\BaseController;
+use App\Controller\HomeController;
+use App\Exception\ActionNotFoundException;
+use App\Exception\ControllerNotFoundException;
+
+class Router {
+
+    /**
+     * @throws ControllerNotFoundException
+     * @throws ActionNotFoundException
+     */
+    public function navigateRoute(): void {
+        $defaultController = 'home';
+
+        $route = empty($_GET['route']) ? $defaultController : $_GET['route'];
+
+        $controllerAction = explode("/", $route);
+
+        $controllerRoute = empty($controllerAction[0]) ? $defaultController : $controllerAction[0];
+        $controllerInst = $this->controllerFactory($controllerRoute);
+
+        $actionRoute = empty($controllerAction[1]) ? $controllerInst->defaultAction : $controllerAction[1];
+        $this->callAction($controllerInst, $actionRoute);
+    }
+
+    public function navigate(string $controllerRoute, string $actionRoute): void {
+        $controllerInst = $this->controllerFactory($controllerRoute);
+        $this->callAction($controllerInst, $actionRoute);
+    }
+
+    public function controllerFactory(string $controllerRoute): BaseController {
+        $controllerQualfName = "App\Controller\\" .  ucfirst($controllerRoute) . "Controller";
+
+        if (!class_exists($controllerQualfName)) {
+            throw new ControllerNotFoundException(
+                "Controller " . $controllerQualfName . " not found.",
+                404
+            );
+        }
+
+        return new $controllerQualfName();
+    }
+
+    public function callAction(BaseController $controllerInst, string $actionRoute): void {
+        $actionQualfName = $actionRoute . "Action";
+
+        if (!method_exists($controllerInst, $actionQualfName)) {
+            throw new ActionNotFoundException(
+                "Action " . $actionQualfName . " not found on controller " . $controllerInst,
+                404
+            );
+        }
+
+        $controllerInst->$actionQualfName();
+    }
+}
